1e8849438a1cfa3791bfda1f35cba3d338d78262
Pasq21 <lefossepasquale@gmail.com>
2019-07-09 17:50:05 +0200

    esercizio 7

---
+++ src/main/java/esercizio7/Palindroma.java
@@ -0,0 +1,28 @@
+package esercizio7;
+
+
+
+public class Palindroma {
+	
+		
+		public static boolean isPalindroma(String x) {
+		int inizio=0;
+		int fine = x.length()-1;
+		int mezzo= fine/2;
+		int i=0;
+		for (i=0; i<=mezzo; i++) {
+			if(x.charAt(inizio)==x.charAt(fine)) {
+				inizio++;
+				fine--;
+			} else
+				break;
+		}
+		if(i>mezzo) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
